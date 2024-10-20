30939eb422e3cf80d91b0cdfc9e3f1b5b123f250
Alonso Herreros <alonso.herreros.c@gmail.com>
2024-10-20 17:12:18 +0200

    Update hyprland_scale script for forcing unstable scales

--- scripts/system/hyprland_scale.sh
+++ scripts/system/hyprland_scale.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # Usage:
-# $ ./hyprland_scale.sh i 5
-# $ ./hyprland_scale.sh d
+# $ ./hyprland_scale.sh -i
+# $ ./hyprland_scale.sh -d
+# $ ./hyprland_scale.sh -s 1.15
 
 readonly SCALES=(1.00 1.20 1.25 1.333333 1.40 1.50 1.60 1.666667 1.875 2.00)
 
@@ -20,27 +21,36 @@ get_scale() {
 
 get_scale_index() {
     scale=$(get_scale)
+    # Default to the first option
+    scale_index=0
     for i in "${!SCALES[@]}"; do
         [[ "${SCALES[$i]}" = "${scale}" ]] && scale_index="$i"
     done
     echo $scale_index
 }
 
+set_force() {
+    [[ -n "$1" ]] && param="$1" || param="true"
+    hyprctl keyword debug:disable_scale_checks $param
+}
+
 set_scale() {
     hyprctl keyword monitor ,preferred,auto,$1
     echo "Monitor scale set to $1"
-
 }
 
 case "$1" in
     -s)
         shift
+        set_force
         set_scale "$1"
         ;;
     -i)
+        set_force "false"
         set_scale "${SCALES[$(get_scale_index)+1]}"
         ;;
     -d)
+        set_force "false"
         set_scale "${SCALES[$(get_scale_index)-1]}"
         ;;
     *)
