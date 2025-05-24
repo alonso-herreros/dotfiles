6b7b650156012b5d3061dccc3a82f3145dd17245
Alonso Herreros <alonso.herreros.c@gmail.com>
2025-05-24 14:22:37 +0200

    refactor: Move media_ctl scripts to scripts dir

--- config/waybar/scripts/update-media-volume.sh
+++ config/waybar/scripts/update-media-volume.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# media-ctl.sh - A script to control media playback using playerctl
-~/.config/waybar/scripts/media-ctl.sh volume "$@"
+~/.scripts/system/media_ctl.sh volume "$@"
 pkill -RTMIN+10 waybar
 sleep 2.2
 pkill -RTMIN+10 waybar
