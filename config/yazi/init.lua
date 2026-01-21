aeb4b3829660ee3735440507a5460792b5302757
Alonso Herreros <alonso.herreros.c@gmail.com>
2026-01-21 17:49:36 +0100

    Revert "yazi(init): Disable yatline"
    This reverts commit fb55b98ebdc1b26d06171f9560966f909242f9e2.

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -32,96 +32,96 @@
 -- end, 500, Status.RIGHT)
 
 -- ==== Yatline ====
---require("yatline"):setup({
---	--theme = my_theme,
---	section_separator = { open = "", close = "" },
---	part_separator = { open = "", close = "" },
---	inverse_separator = { open = "", close = "" },
+require("yatline"):setup({
+	--theme = my_theme,
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
+	inverse_separator = { open = "", close = "" },
 
---	style_a = {
---		fg = "white",
---		bg_mode = {
---			normal = "blue",
---			select = "brightyellow",
---			un_set = "brightred"
---		}
---	},
---	style_b = { bg = "brightblack", fg = "brightwhite" },
---	style_c = { bg = "black", fg = "brightwhite" },
+	style_a = {
+		fg = "white",
+		bg_mode = {
+			normal = "blue",
+			select = "brightyellow",
+			un_set = "brightred"
+		}
+	},
+	style_b = { bg = "brightblack", fg = "brightwhite" },
+	style_c = { bg = "black", fg = "brightwhite" },
 
---	permissions_t_fg = "green",
---	permissions_r_fg = "yellow",
---	permissions_w_fg = "red",
---	permissions_x_fg = "cyan",
---	permissions_s_fg = "white",
+	permissions_t_fg = "green",
+	permissions_r_fg = "yellow",
+	permissions_w_fg = "red",
+	permissions_x_fg = "cyan",
+	permissions_s_fg = "white",
 
---	tab_width = 20,
---	tab_use_inverse = false,
+	tab_width = 20,
+	tab_use_inverse = false,
 
---	selected = { icon = "󰻭", fg = "yellow" },
---	copied = { icon = "", fg = "green" },
---	cut = { icon = "", fg = "red" },
+	selected = { icon = "󰻭", fg = "yellow" },
+	copied = { icon = "", fg = "green" },
+	cut = { icon = "", fg = "red" },
 
---	total = { icon = "󰮍", fg = "yellow" },
---	succ = { icon = "", fg = "green" },
---	fail = { icon = "", fg = "red" },
---	found = { icon = "󰮕", fg = "blue" },
---	processed = { icon = "󰐍", fg = "green" },
+	total = { icon = "󰮍", fg = "yellow" },
+	succ = { icon = "", fg = "green" },
+	fail = { icon = "", fg = "red" },
+	found = { icon = "󰮕", fg = "blue" },
+	processed = { icon = "󰐍", fg = "green" },
 
---	show_background = true,
+	show_background = true,
 
---	display_header_line = true,
---	display_status_line = true,
+	display_header_line = true,
+	display_status_line = true,
 
---	component_positions = { "header", "tab", "status" },
+	component_positions = { "header", "tab", "status" },
 
---	header_line = {
---		left = {
---			section_a = {
---				{type = "line", custom = false, name = "tabs", params = {"left"}},
---			},
---			section_b = {
---			},
---			section_c = {
---			}
---		},
---		right = {
---			section_a = {
---			},
---			section_b = {
---			},
---			section_c = {
---				{type = "coloreds", custom = false, name = "count"},
---			}
---		}
---	},
+	header_line = {
+		left = {
+			section_a = {
+				{type = "line", custom = false, name = "tabs", params = {"left"}},
+			},
+			section_b = {
+			},
+			section_c = {
+			}
+		},
+		right = {
+			section_a = {
+			},
+			section_b = {
+			},
+			section_c = {
+				{type = "coloreds", custom = false, name = "count"},
+			}
+		}
+	},
 
---	status_line = {
---		left = {
---			section_a = {
---				{type = "string", custom = false, name = "tab_mode"},
---			},
---			section_b = {
---				{type = "string", custom = false, name = "tab_path", params = { trimed = false, max_length=24, trim_length=10 }},
---			},
---			section_c = {
---				-- {type = "string", custom = false, name = "hovered_name", params = { trimed = false, show_symlink = true, max_length=24, trim_length=10 }},
---			}
---		},
---		right = {
---			section_a = {
---				{type = "string", custom = false, name = "cursor_position"},
---			},
---			section_b = {
---				-- {type = "string", custom = false, name = "hovered_mime"},
---				{type = "string", custom = false, name = "hovered_size"},
---			},
---			section_c = {
---				{type = "coloreds", custom = false, name = "permissions"},
---			}
---		}
---	},
---})
+	status_line = {
+		left = {
+			section_a = {
+				{type = "string", custom = false, name = "tab_mode"},
+			},
+			section_b = {
+				{type = "string", custom = false, name = "tab_path", params = { trimed = false, max_length=24, trim_length=10 }},
+			},
+			section_c = {
+				-- {type = "string", custom = false, name = "hovered_name", params = { trimed = false, show_symlink = true, max_length=24, trim_length=10 }},
+			}
+		},
+		right = {
+			section_a = {
+				{type = "string", custom = false, name = "cursor_position"},
+			},
+			section_b = {
+				-- {type = "string", custom = false, name = "hovered_mime"},
+				{type = "string", custom = false, name = "hovered_size"},
+			},
+			section_c = {
+				{type = "coloreds", custom = false, name = "permissions"},
+			}
+		}
+	},
+})
 
 -- ==== GIT ====
 th = th or {}
