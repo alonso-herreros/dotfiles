914af15b2e3e408e334e5bbfb790d0451359f19d
Alonso Herreros <alonso.herreros.c@gmail.com>
2026-03-10 13:19:34 +0100

    shell-env: Add include guard only where necessary
    Only around non-idempotent changes

--- config/shell/env.sh
+++ config/shell/env.sh
@@ -5,46 +5,45 @@
 #
 #     . ~/.config/shell/env.sh
 
+
+# ===== Options ============================================
+export LANG=en_US.utf8
+
+
+# ===== Paths ==============================================
+# ----- Search paths -----------------------------
 # An include guard, since this is technically not idempotent
-if [ -z "$INCLUDED_ENV" ]; then
-    INCLUDED_ENV=1
-
-    # ===== Options ============================================
-    export LANG=en_US.utf8
-
-
-    # ===== Paths ==============================================
-    # Search paths
+if [ -z "$INCLUDED_ENV_PATHS" ]; then
+    INCLUDED_ENV_PATHS=1
     [ -d "$HOME/.local/bin" ] && PATH="$HOME/.local/bin:$PATH"
     [ -d "$HOME/.local/lib/locale" ] && export LOCPATH="$HOME/.local/lib/locale:$LOCPATH"
-
-    # XDG
-    [ -z "$XDG_CONFIG_HOME" ] && export XDG_CONFIG_HOME="$HOME/.config"
-    [ -z "$XDG_DATA_HOME" ]   && export XDG_DATA_HOME="$HOME/.local/share"
-    [ -z "$XDG_STATE_HOME" ]  && export XDG_STATE_HOME="$HOME/.local/state"
-    [ -z "$XDG_CACHE_HOME" ]  && export XDG_CACHE_HOME="$HOME/.cache"
-
-    export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
-
-
-    # ===== Programs and options ===============================
-    if command -v vim >/dev/null 2>&1; then
-        export EDITOR="vim"
-    fi
-
-    if command -v less >/dev/null 2>&1; then
-        export PAGER="less"
-        export LESS="-Ri"
-    fi
-
-    if command -v ghdl >/dev/null 2>&1; then
-        export GHDL="$(command -v ghdl)"
-    fi
-
-
-    # ===== Default user and hostname ==========================
-    # We need to check if the variable is set to avoid overriding it
-    [ -z "$DEFAULT_USER" ] && export DEFAULT_USER="$(whoami)"
-    [ -z "$DEFAULT_HOSTNAME" ] && export DEFAULT_HOSTNAME="$(hostname)"
-
 fi
+
+# ----- XDG --------------------------------------
+[ -z "$XDG_CONFIG_HOME" ] && export XDG_CONFIG_HOME="$HOME/.config"
+[ -z "$XDG_DATA_HOME" ]   && export XDG_DATA_HOME="$HOME/.local/share"
+[ -z "$XDG_STATE_HOME" ]  && export XDG_STATE_HOME="$HOME/.local/state"
+[ -z "$XDG_CACHE_HOME" ]  && export XDG_CACHE_HOME="$HOME/.cache"
+
+export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+
+
+# ===== Programs and options ===============================
+if command -v vim >/dev/null 2>&1; then
+    export EDITOR="vim"
+fi
+
+if command -v less >/dev/null 2>&1; then
+    export PAGER="less"
+    export LESS="-Ri"
+fi
+
+if command -v ghdl >/dev/null 2>&1; then
+    export GHDL="$(command -v ghdl)"
+fi
+
+
+# ===== Default user and hostname ==========================
+# We need to check if the variable is set to avoid overriding it
+[ -z "$DEFAULT_USER" ] && export DEFAULT_USER="$(whoami)"
+[ -z "$DEFAULT_HOSTNAME" ] && export DEFAULT_HOSTNAME="$(hostname)"
